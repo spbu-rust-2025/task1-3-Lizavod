9543b659d829979172643702c1259acc949d9059
Елизавета Водолазская <elizavetavxa@gmail.com>
2025-10-26 23:44:19 +0300

    added file opening

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
+use std::fs::File;
+use std::path::Path;
 
+fn read_string() -> String {
+    let mut string = String::new();
+    std::io::stdin().read_line(&mut string)
+        .ok()
+        .expect("failure");
+    return string;
+}
+
+fn main() {
+    let file_path = read_string();
+    let path = Path::new(file_path.trim());
+    let result = File::open(path);
+    match result {
+        Ok(_) => println!("success"),
+        Err(_) => println!("failure")
+    }
 }
